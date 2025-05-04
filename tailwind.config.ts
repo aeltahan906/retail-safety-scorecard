9add1ae138948f40dd97dcddca8e645a7c9ecb3e
aeltahan906 <ahmed.eltahan@chalhoub.com>
2025-05-05 00:26:54 +0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-
+ 
 import type { Config } from "tailwindcss";
 
 export default {
@@ -73,9 +73,9 @@ export default {
 				}
 			},
 			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
+				lg: 'var(--radius, 0.5rem)',
+				md: 'calc(var(--radius, 0.5rem) - 2px)',
+				sm: 'calc(var(--radius, 0.5rem) - 4px)'
 			},
 			keyframes: {
 				'accordion-down': {
